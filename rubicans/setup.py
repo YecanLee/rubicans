394ca79cc1b1c82fa1090828c1cc026fc8bedac7
Li <M.Li@campus.lmu.de>
2023-12-10 02:56:21 +0100

    Update ddpm denoising model sample

---
+++ rubicans/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo for deep learning paper simple implementation',
+    author='Meimingwei Li',
+    license='MIT',
+)
