af74f4328554ce95087d65a7449f1dc4739c2b7c
Li <M.Li@campus.lmu.de>
2023-12-12 23:56:53 +0100

    Create different parts of SD, prepare to implement

---
+++ rubicans/models/stable_diffusion_model.py
@@ -0,0 +1,18 @@
+from typing import Option, List
+
+import torch
+import torch.nn as nn
+
+class AutoEncoder(nn.Module):
+    def __init__(self, *args, **kargs):
+        super().__init__()
+
+    def forward(self, *args, **kargs):
+        pass
+
+class Unet(nn.Module):
+    def __init__(self, *args, **kargs):
+        super().__init__()
+
+    def forward(self, *args, **kargs):
+        pass
