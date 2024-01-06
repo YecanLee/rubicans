d2587617131216ea774f5928a484ddc67a5c745d
Li <M.Li@campus.lmu.de>
2024-01-06 23:20:00 +0100

    Finish DDPM Scheduler

--- rubicans/diffusers_set/ddpm_scheduler.py
+++ rubicans/diffusers_set/ddpm_scheduler.py
@@ -9,7 +9,7 @@ from typing import List, Union
 
 from typing import Optional
 
-from utils import BaseOutput
+from utils import BaseOutput, randn_tensor
 
 # try to use the @measure_time decorator to measure the time of the function
 
@@ -22,10 +22,10 @@ class DDPMSchedulerOutput(BaseOutput):
     The prev_sample is the previous timestample x_{t-1} and this should also be the input of next step
     The prev_sample_output is the previous time step denoising output x_{0} , this can be used for checking and also for guidance
     """
-    prev_sample = torch.FloatTensor
-    prev_sample_output = Optional[torch.FloatTensor] = None
+    prev_sample : torch.FloatTensor
+    prev_sample_output : Optional[torch.FloatTensor] = None
 
-def rescale_alphas_terminal(betas: int) -> int:
+def rescale_alphas_terminal(betas: torch.FloatTensor) -> torch.FloatTensor:
     """
     This function is based on the paper "Common Diffusion Noise Schedules and Sample Steps are flawed,
     In that paper, the author argues that the diffusion process should be rescaled at the terminal step,
@@ -49,6 +49,7 @@ def rescale_alphas_terminal(betas: int) -> int:
     alphas_bar = alphas_t[1:] / alphas_t[:-1]
     alphas = torch.cat([alphas_0_bar.unsqueeze(0), alphas_bar])
     betas = 1 - alphas
+    return betas
 
 def betas_for_alpha_bar(
     num_diffusions_steps:int,
@@ -65,9 +66,9 @@ def betas_for_alpha_bar(
     The beta would be used to calculate the noise level for the current step, it becomes larger and larger as the diffusion process goes on
     """
     if alpha_transform_type == 'cosine':
-        return 
+        pass
 
-class DDPMSchduler(SchedulerMixin, ConfigMixin):
+class DDPMSchduler(nn.Module):
     """
     The SchedulerMixin and ConfigMixin are used to store the results of the scheduler
     variance: clip the variance of the noise to control the noise level
@@ -80,7 +81,7 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
                  beta_start: float = 0.0001,
                  beta_end: float = 0.02,
                  beta_scheduler: str = "linear",
-                 trained_betas: Optional[Union[np.narray, List[float]]] = None,
+                 trained_betas: Optional[Union[np.ndarray, List[float]]] = None,
                  variance: str = 'fixed_small',
                  clip_sample: bool = True,
                  clip_sample_threshold: float = 1.0,
@@ -90,7 +91,7 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
                  sample_max_value: float = 1.0,
                  timestep_spacing: str = 'leading',
                  steps_offset: int = 0,
-                 rescale_betas_zero_sbr: int = False
+                 rescale_betas_zero_sbr: bool = False,
                  ):
         if trained_betas is not None:
             self.betas = torch.Tensor(trained_betas, dtype=torch.float32)
@@ -109,12 +110,17 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
         if rescale_alphas_terminal:
             self.betas = rescale_alphas_terminal(self.betas)
         
+        self.prediction_type = prediction_type
+        self.clip_sample_threshold = clip_sample_threshold
+        
         self.variance = variance
         self.clip_sample = clip_sample
 
+        self.thresholding = thresholding
+
         self.alphas =  1 - self.betas
         self.alphas_bar = torch.cumprod(self.alphas, dim = 0)
-        self.one = torch.Tensor(1.0)
+        self.one = torch.tensor(1.0)
 
         self.init_noise_sigma = 1.0
         self.num_training_steps = num_training_steps    
@@ -164,9 +170,9 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
                 self.custom_timesteps = True
             else:
                 if num_inference_steps > self.num_training_steps:
-                    raise ValueError(f'The inference steps: {num_inference_steps} must be smaller than the training timesteps:
+                    raise ValueError(f"""The inference steps: {self.num_inference_steps} must be smaller than the training timesteps:
                                      {self.num_training_steps} as the UNet model with this specific schduler is only capaple of 
-                                     dealing with this number of denoising timesteps: {self.num_training_steps}')
+                                     dealing with this number of denoising timesteps: {self.num_training_steps}""")
 
                 self.num_inference_steps = num_inference_steps
                 self.custom_timesteps = False
@@ -187,8 +193,8 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
                 factor = self.num_training_steps/self.num_inference_steps
                 timesteps = np.round(np.arange(self.num_training_steps, 0, -factor)).astype(np.int64)
             else:
-                raise ValueError(f"The timestep spacing {self.timestep_spacing} is not supported yet,
-                                  make sure to choose one of them from 'linspace', 'leading', 'trailing'.")
+                raise ValueError(f"""The timestep spacing {self.timestep_spacing} is not supported yet,
+                                  make sure to choose one of them from 'linspace', 'leading', 'trailing'.""")
         
         # put the timesteps onto the Gpu
         self.timesteps = torch.from_numpy(timesteps).to(device)
@@ -199,7 +205,26 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
         During the denoising process, if the pixel value is larger than 1, and the threshold is s, then we will threshold the pixel value between [-s, s]
         Then we will divide the pixel value by s, and then add 1 to the pixel value, this will prevent the pixel value from being saturated
         """
-        pass
+        dtype = x.dtype
+        device = x.device
+        batch, channel, *remained_dimensions = x.shape
+
+        if dtype not in (torch.float32, torch.float64):
+            x = x.float()
+        
+        x = x.reshape(batch, channel * np.prod(remained_dimensions))
+        abs_x = torch.abs(x)
+
+        s = torch.quantile(abs_x, self.dynamic_thresholding_ratio, dim=1, keepdim=True)
+        s = torch.clamp(s, min=1, max=self.sample_max_value)
+
+        s = s.unsqueeze(-1) # broadcast the s to the same shape as the x, now the shape of s is [batch_size, 1]
+        x = torch.clamp(x, min=-s, max=s)/s # threshold to [-s, s], then divide by s, now the x is in [-1, 1]
+
+        x = x.reshape(batch, channel, *remained_dimensions)
+        x = x.to(dtype=dtype)
+        
+        return x
 
     def _get_variance(self, t: int, predicted_variance:Optional[str]=None, variance_type:Optional[str]=None):
         """
@@ -228,7 +253,7 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
         elif variance_type == 'learned':
             return predicted_variance
         else:
-            variance_type == 'learned_range':
+            variance_type == 'learned_range'
             min_variance = torch.log(variance)
             max_variance = torch.log(beta_t)
             frac = (predicted_variance + 1)/2
@@ -266,9 +291,9 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
         
         if self.prediction_type == 'epsilon':
             pred_original_sample = x - ((beta_prod_t) ** 0.5) * model_output/alpha_prod_t ** 0.5 # this is the function 15 in the DDPM paper
-        if self.prediction_type == 'sample':
+        elif self.prediction_type == 'sample':
             pred_original_sample = model_output
-        if self.prediction_type == 'variance':
+        elif self.prediction_type == 'variance':
             pred_original_sample = (alpha_prod_t ** 0.5) * x - (beta_prod_t ** 0.5) * model_output
         else:
             raise ValueError(f'The prediction type {self.prediction_type} is not supported yet, please choose one of them from "epsilon", "sample", "variance"')
@@ -282,7 +307,94 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
         # computer the coefficient for the noise, check the function 7 in the DDPM paper
         pred_original_sample_coefficient = (alpha_prod_t_prev ** 0.5 * beta_prod_t) / beta_prod_t
         current_sample_coefficient = (alpha_prod_t ** 0.5 * beta_prod_t_prev) / beta_prod_t
+
+        # calculate the DDPM paper function 7, alert, this is different from function 6, the function 6 will be implemented in the next step
+        pred_prev_sample = pred_original_sample_coefficient * pred_original_sample + current_sample_coefficient * x
+
+        # use the DDPM paper function 6 to calculate the x_{t-1}
+        # add noise, aka the variance
+        variance = 0
+        if t > 0:
+            device = model_output.device
+            variance_noise = randn_tensor(model_output.shape, generator=generator, device=device, dtype=model_output.dtype)
+            if self.variance_type == 'fixed_log_small':
+                variance = self._get_variance(t, predicted_variance) * variance_noise
+                variance = torch.exp(0.5 * variance) * variance_noise
+            elif self.variance_type == 'learned_range':
+                variance = self._get_variance(t, predicted_variance) * variance_noise
+            else:
+                variance = (self._get_variance(t, predicted_variance) ** 0.5 ) * variance_noise
         
+        pred_prev_sample = pred_prev_sample + variance
+        if not return_dict:
+            return (pred_prev_sample, )
+        
+        return DDPMSchedulerOutput(prev_sample=pred_prev_sample, prev_sample_output=pred_original_sample)
+    
+    def add_noise(self, 
+                  x:torch.FloatTensor, 
+                  noise: torch.FloatTensor, 
+                  t: torch.IntTensor
+                  ) -> torch.FloatTensor:
+        """
+        torch.Tensor is the main tensor class in PyTorch. When you create a torch.Tensor without specifying a data type, 
+        it defaults to float32. So, torch.Tensor is equivalent to torch.FloatTensor if you don't specify a data type.
+        Args:
+            x: the input of the diffusion process, aka the original sampled image
+            noise: the noise to be added to the input
+            t: the current timestep
+        """
+        # always make sure the alpha_cumprod has the same device and dtype as the sample x
+        alpha_cumprod = self.alphas_bar.to(device=x.device, dtype=x.dtype)
+        t = t.to(device=x.device, dtype=x.dtype)
+        
+        squd_alpha_cumprod = math.sqrt(alpha_cumprod[t])
+        # flatten the noise to make sure the noise has the same shape as the sample x
+        squd_alpha_cumprod = squd_alpha_cumprod.flatten() # now it is a 1D tensor
+        while len(squd_alpha_cumprod.shape) < len(x.shape):
+            squd_alpha_cumprod = squd_alpha_cumprod.unsqueeze(-1) # now the noise tensor has the same shape as the sample x
+        
+        squd_one_minus_alpha_cumprod = math.sqrt(1 - alpha_cumprod[t])
+        squd_one_minus_alpha_cumprod = squd_one_minus_alpha_cumprod.flatten()
+        while len(squd_one_minus_alpha_cumprod.shape) < len(x.shape):
+            squd_one_minus_alpha_cumprod = squd_one_minus_alpha_cumprod.unsqueeze(-1)
+
+        # This is the function 4 in the DDPM paper
+        x_t = squd_alpha_cumprod * x + squd_one_minus_alpha_cumprod * noise
+        return x_t
+
+    def get_velocity(self,
+                     x: torch.FloatTensor,
+                     t: torch.IntTensor,
+                     noise: torch.FloatTensor
+                     ) -> torch.FloatTensor:
+        """
+        A function to calculate the velocity of the diffusion process
+        Args:
+            x: the input of every steps in the diffusion process
+            t: the current timestep
+            noise: the noise to be added to the input
+        """
+        alpha_cumprod = self.alphas_bar.to(device=x.device, dtype=x.dtype)
+        t = t.to(device=x.device, dtype=x.dtype)
+        
+        squd_alpha_cumprod = math.sqrt(alpha_cumprod[t])
+        # flatten the noise to make sure the noise has the same shape as the sample x
+        squd_alpha_cumprod = squd_alpha_cumprod.flatten() # now it is a 1D tensor
+        while len(squd_alpha_cumprod.shape) < len(x.shape):
+            squd_alpha_cumprod = squd_alpha_cumprod.unsqueeze(-1) # now the noise tensor has the same shape as the sample x
+        
+        squd_one_minus_alpha_cumprod = math.sqrt(1 - alpha_cumprod[t])
+        squd_one_minus_alpha_cumprod = squd_one_minus_alpha_cumprod.flatten()
+        while len(squd_one_minus_alpha_cumprod.shape) < len(x.shape):
+            squd_one_minus_alpha_cumprod = squd_one_minus_alpha_cumprod.unsqueeze(-1)
+        
+        velocity = squd_alpha_cumprod * noise - squd_one_minus_alpha_cumprod * x
+        return velocity
+    
+    def __len__(self):
+        return self.num_training_steps
+
     def previous_timestep(self, t:Optional[int]=None) -> int:
         """
         function used to get the previous timestep, prepared for the DDPM scheduler, check the function 6 and 7 in the paper
@@ -301,3 +413,46 @@ class DDPMSchduler(SchedulerMixin, ConfigMixin):
             num_inference_steps = (self.num_inference_steps if self.num_inference_steps else self.num_training_steps)
             prev_t = t - self.num_training_steps//num_inference_steps
             return prev_t
+
+class SimpleCNN(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, out_channels, kernel_size=3, padding=1)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        x = self.activation(self.conv1(x))
+        x = self.activation(self.conv2(x))
+        x = self.conv3(x)
+        return x
+
+# Instantiate the simple CNN model
+model = SimpleCNN()
+
+# Create a random input tensor (e.g., 1 image, 3 color channels, 128x128 pixels)
+input_tensor = torch.randn(1, 3, 128, 128)
+
+# Initialize the diffusion scheduler
+scheduler = DDPMSchduler(prediction_type='epsilon')
+
+# Simulate the diffusion steps
+# Here, we'll just demonstrate a single step for simplicity
+timestep = 0
+
+# Pass the input tensor through the CNN
+output = model(input_tensor)
+
+# Print the output shape to verify
+print("Output shape from CNN:", output.shape)
+
+
+# Normally, you would generate noise and apply it to your data, but for simplicity, we use the model output
+noise = torch.randn_like(output)
+
+# Simulate a single step of the diffusion process
+scheduler_output = scheduler.step(model_output=noise, t=timestep, x=input_tensor)
+
+# Print the output to verify
+print("Scheduler output:", scheduler_output)
